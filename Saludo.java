8ea38f07029f1a3470218eef5039d8b4215e5982
nahun salez <nahun@192.168.0.18>
2020-11-11 20:22:13 -0600

    Primer comit para repositorio remoto -

---
+++ Saludo.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author walter nahun salez
+ */
+public class Saludo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        System.out.println("Hola Soy de la clase de Programacion Orintado a Objetos");
+    }
+}
