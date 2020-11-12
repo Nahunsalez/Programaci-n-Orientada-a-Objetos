85f22bf208938841a861f14d2d3322d5987d61a2
nahun salez <nahun@192.168.0.18>
2020-11-11 20:22:02 -0600

    Primer comit para repositorio remoto -

---
+++ verificar.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author walter nahun salez
+ */
+public class verificar {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        int M = 6,T = 1,K = -10;
+        if(M>T){
+           System.out.println("M>T es Verdadero");
+        }else{
+            System.out.println("M>T es Falso");
+        }
+        if(T/K==-5){
+            System.out.println("T/K==-5 es Verdadero");
+        }else{
+            System.out.println("T/K==-5 es False");
+        }
+        if((M+T==7)||(M-T==5)){
+            System.out.println("(M+T==7)||(M-T==5) es Verdadero");
+        }else{
+            System.out.println("(M+T==7)||(M-T==5) es Falso");
+        }
+    }
+}
