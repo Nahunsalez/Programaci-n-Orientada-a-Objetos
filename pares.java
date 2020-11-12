59944d5c9c75c327d25c148f96e9bda9bf6f43be
nahun salez <nahun@192.168.0.18>
2020-11-11 20:22:29 -0600

    Primer comit para repositorio remoto -

---
+++ pares.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author walter nahun salez
+ */
+public class pares {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        // TODO code application logic here
+        for( int par=1; par<=100; par++)
+        {
+            if(par%2==0)
+            {
+                System.out.println(par);
+            }
+        }
+    }
+}
